69565e70dd7fc69092c36084031403d4c9b16c5f
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-07-22 18:03:56 +0700

    driver.h is crossplatform

---
+++ driver.h
@@ -0,0 +1,25 @@
+#ifndef _PLATFORM_H_
+#define _PLATFORM_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <alsa/asoundlib.h>
+
+void system_init(void);
+
+void driver_init(
+	int SMPR); // Sampling Rate
+
+void sample_prep(
+	double FR, // Frequency (Hz)
+	double DUR, //Duration (s)
+	int AMP); //Amplitudo
+
+void play_wave(void);
+
+void system_loop(void);
+
+#endif
+
