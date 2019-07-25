5b020e43c0c094a9d641f24ae295004d60d775e8
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-07-25 12:54:42 +0700

    remove debug print

--- testarray/driver.c
+++ testarray/driver.c
@@ -3,7 +3,6 @@
 #include "../driver.h"
 
 #define NUM_SAMPLES     SAMPLING_RATE
-#undef DEBUG_PRINT
 
 uint16_t sine[NUM_SAMPLES];
 uint16_t sample;
@@ -30,7 +29,7 @@ void sample_prep(
 	uint8_t stop = 0;
 	uint16_t i = 1;
 
-	sine[0] = 0;
+	sine[0] = 2000;
 
 	while(stop==0){
 		x = (double) i / (double) SAMPLING_RATE;
@@ -38,9 +37,7 @@ void sample_prep(
 		sample = 2000 * y;
 
 		sine[i] = sample;
-#ifdef DEBUG_PRINT
-		printf("%4i ",sample);
-#endif
+
 		i++;
 
 		if(sample == 2000){ phase++;}
@@ -48,40 +45,17 @@ void sample_prep(
 		else if(sample < 2000){ neg_b = 1;	}
 
 		if(neg_b != neg_a){
-#ifdef DEBUG_PRINT
-			printf("negA %i negB %i ",neg_a,neg_b);
-			printf("changes detected\n");
-#endif
 			phase=phase+1;
-			if(phase==2){
-				onesample = i;
-				stop=1;
-			}
+			if(phase==2){ onesample = i;stop=1;	}
 			neg_a = neg_b;
 		}
-		else if(neg_b == neg_a){
-#ifdef DEBUG_PRINT
-			printf("negA %i negB %i\n",neg_a,neg_b);
-#endif
-			neg_a = neg_b;
-		}
+		else if(neg_b == neg_a){ neg_a = neg_b; }
 
-		if(i==NUM_SAMPLES){
-			stop=1;
-#ifdef DEBUG_PRINT
-			printf("Max array reached\n");
-#endif
-		}
+		if(i==NUM_SAMPLES){	stop=1;	}
 	};
-#ifdef DEBUG_PRINT
-	printf("phase %i stop %i\n",phase,stop);
-#endif
 }
 
 void play_wave(void){
-#ifndef DEBUG_PRINT
-	for(uint16_t i=0,i<onesample;i++){
-		printf("%4i ",sine[i]);
-	}
-#endif
+	//only print sine wave
+	for(uint16_t i=0;i<onesample;i++){	printf("%4i, ",sine[i]); }
 }
