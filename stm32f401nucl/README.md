03bcd0009679b5b0b4cdafd448e8f9ebf8b5ea70
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-07-25 22:13:33 +0700

    stm32f401re nucleo simple wav seems ok

---
+++ stm32f401nucl/README.md
@@ -0,0 +1,41 @@
+This platform is for STM32F429ZI Discovery board.
+
+Software Requirement:
+- arm-none-eabi GCC toolchain
+- GNU/Make
+- ChibiOS/RT version 3.x
+- stlink or stm32flash
+
+on Arch Linux, install using command:
+~~~
+sudo pacman -S arm-none-eabi-gcc arm-none-eabi-newlib stlink
+~~~
+
+Hardware Requirement:
+- STM32F401RE Discovery board.
+- Audio DAC using I2S interface (e.g MAX98357A)
+Connection:
+~~~
+STM32	MAX98
+----	----
+GND		GND
+3V		Vin
+3V		SD (Shutdown)
+PC3		Din (Data)
+PB10	BCLK
+PB12	LRC
+~~~
+
+For the record, there's range on:
+- Frequency: from 500 to 10000 (not calibrated in Hz)
+- Amplitudo: from 10 to 10000 (not calibrated in dB)
+
+To build, just run on top repos directory:
+~~~
+./build.sh stm32f401nucl
+~~~
+
+If no error on compiling, connect board to usb then run:
+~~~
+sudo st-flash write build/stm32f401nucl/build/pikotes.bin 0x8000000
+~~~
--- stm32f401nucl/driver.c
+++ stm32f401nucl/driver.c
@@ -3,6 +3,22 @@
 
 #include "../driver.h"
 
+#define NUM_CHANNELS    2
+#define NUM_SAMPLES     512
+#define I2S_BUF_SIZE    NUM_SAMPLES*NUM_CHANNELS
+
+static uint16_t sine_sample[I2S_BUF_SIZE];
+static uint8_t play_duration;
+
+I2SConfig i2scfg = {
+  sine_sample,
+  NULL,
+  NUM_SAMPLES, // should be buffer size and size_t are big enough on gcc
+  NULL,
+  0,
+  16
+};
+
 static THD_WORKING_AREA(waLed1, 128);
 static THD_FUNCTION(thdLed1, arg) {
 
@@ -30,7 +46,9 @@ void system_init(void){
 }
 
 void driver_init(void){
-    
+	palSetPadMode(GPIOB, 12, PAL_MODE_ALTERNATE(5));
+    palSetPadMode(GPIOB, 10, PAL_MODE_ALTERNATE(5));
+    palSetPadMode(GPIOC, 3 , PAL_MODE_ALTERNATE(5));
 }
 
 void sample_prep(
@@ -38,11 +56,54 @@ void sample_prep(
 	double DUR, //Duration (s)
 	int AMP) //Amplitudo
 {
-	
+	double x,y;
+
+	uint8_t neg_a,neg_b;
+	uint8_t phase = 0;
+	uint8_t stop = 0;
+	uint16_t sample;
+
+	uint16_t i = 1;
+	sine_sample[0] = AMP * 0.2;
+
+	play_duration = DUR;
+
+	while(stop==0){
+		x = (double) i / (double) SAMPLING_RATE;
+		y = sin(2.0 * 3.14159 * FR * x) + 1;
+		sample = AMP * 0.2 * y;
+
+		sine_sample[i] = sample;
+		if(NUM_CHANNELS==2){	sine_sample[i+1] = sample; }
+
+		i++;
+
+		if(sample == 2000){ phase++;}
+		else if(sample > 2000){ neg_b = 0;	}
+		else if(sample < 2000){ neg_b = 1;	}
+
+		if(neg_b != neg_a){
+			phase=phase+1;
+			if(phase==2){ ;stop=1;	}
+			neg_a = neg_b;
+		}
+		else if(neg_b == neg_a){ neg_a = neg_b; }
+
+		if(i==NUM_SAMPLES){	stop=1;	}
+	};
+
+	//buffer size
+	i2scfg.size = 10*i;
 }
 
 void play_wave(void){
+	i2sStart(&I2SD2, &i2scfg);
+	i2sStartExchange(&I2SD2);
 
+	chThdSleepMilliseconds(play_duration * 100);
+
+	i2sStopExchange(&I2SD2);
+	i2sStop(&I2SD2);
 }
 
 void system_loop(void){
