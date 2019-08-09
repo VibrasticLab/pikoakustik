7803aa5f77f5576a7184de18f09a0d20f474f70b
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-08-10 01:25:27 +0700

    working on led indicator

--- stm32f401nucl/driver.c
+++ stm32f401nucl/driver.c
@@ -3,6 +3,8 @@
 
 #include "../driver.h"
 
+//================================================================================
+
 #define NUM_CHANNELS    2
 #define NUM_SAMPLES     512
 #define I2S_BUF_SIZE    NUM_SAMPLES*NUM_CHANNELS
@@ -10,7 +12,7 @@
 static uint16_t sine_sample[I2S_BUF_SIZE];
 static uint8_t play_duration;
 
-I2SConfig i2scfg = {
+static I2SConfig i2scfg = {
   sine_sample,
   NULL,
   NUM_SAMPLES, // should be buffer size and size_t are big enough on gcc
@@ -18,6 +20,8 @@ I2SConfig i2scfg = {
   0,
   16
 };
+
+//================================================================================
 
 static THD_WORKING_AREA(waLed1, 128);
 static THD_FUNCTION(thdLed1, arg) {
@@ -34,14 +38,25 @@ static THD_FUNCTION(thdLed1, arg) {
 
 static void led_start(void){
     palSetPadMode(GPIOA,5,PAL_MODE_OUTPUT_PUSHPULL);
-
     chThdCreateStatic(waLed1, sizeof(waLed1),	NORMALPRIO, thdLed1, NULL);
 }
 
-static void testWave(void){
-	extChannelDisable(&EXTD1, 13);
-	play_wave();
-	extChannelEnable(&EXTD1, 13);
+//================================================================================
+
+static u_int8_t testWave=0;
+
+static THD_WORKING_AREA(waTestWave, 128);
+static THD_FUNCTION(thdTestWave, arg) {
+
+  (void)arg;
+  chRegSetThreadName("test wave");
+  while (true) {
+      if(testWave==1){
+          play_wave();
+          testWave=0;
+      }
+      chThdSleepMilliseconds(100);
+  }
 }
 
 static void extTestWave(EXTDriver *extp, expchannel_t channel) {
@@ -49,7 +64,7 @@ static void extTestWave(EXTDriver *extp, expchannel_t channel) {
   (void)extp;
   (void)channel;
 
-  testWave();
+  testWave=1;
 }
 
 static const EXTConfig extcfg = {
@@ -80,16 +95,118 @@ static const EXTConfig extcfg = {
   }
 };
 
+static void exti_start(void){
+    palSetPadMode(GPIOC, 13, PAL_MODE_INPUT_PULLUP);
+    extStart(&EXTD1, &extcfg);
+    extChannelEnable(&EXTD1, 13);
+    chThdCreateStatic(waTestWave, sizeof(waTestWave),	NORMALPRIO, thdTestWave, NULL);
+}
+
+//================================================================================
+
+#define LED_TRUE    0 //PA.0
+#define LED_FALSE   1 //PA.1
+#define LED_ANSA    4 //PA.4
+#define LED_ANSB    0 //PB.0
+
+#define LED_AMPL    2 //PA.2
+#define LED_FREQ    3 //PA.3
+
+#define LED_M1      3 //PB.3
+#define LED_M2      5 //PB.5
+#define LED_M3      4 //PB.4
+#define LED_M4      13 //PB.13
+#define LED_M5      14 //PB.14
+
+static void indicator_m_off(void){
+    palClearPad(GPIOA,LED_AMPL);
+    palClearPad(GPIOA,LED_FREQ);
+
+    palSetPad(GPIOB,LED_M1);
+    palSetPad(GPIOB,LED_M2);
+    palSetPad(GPIOB,LED_M3);
+    palSetPad(GPIOB,LED_M4);
+    palSetPad(GPIOB,LED_M5);
+}
+
+static void indicator_m_on(u_int8_t lednum){
+    switch(lednum){
+        case 1: palClearPad(GPIOB,LED_M1);break;
+        case 2: palClearPad(GPIOB,LED_M2);break;
+        case 3: palClearPad(GPIOB,LED_M3);break;
+        case 4: palClearPad(GPIOB,LED_M4);break;
+        case 5: palClearPad(GPIOB,LED_M5);break;
+    }
+}
+
+static THD_WORKING_AREA(waIndicator, 128);
+static THD_FUNCTION(thdIndicator, arg) {
+
+  (void)arg;
+  chRegSetThreadName("indicator run");
+
+  indicator_m_off();
+
+  while (true) {
+      palSetPad(GPIOA,LED_AMPL);
+      indicator_m_on(1);
+      chThdSleepMilliseconds(100);
+  }
+}
+
+static THD_WORKING_AREA(waTestLed, 128);
+static THD_FUNCTION(thdTestLed, arg) {
+
+  (void)arg;
+  chRegSetThreadName("test led");
+
+  palSetPad(GPIOA,LED_TRUE);
+  palClearPad(GPIOA,LED_FALSE);
+  palClearPad(GPIOA,LED_ANSA);
+  palSetPad(GPIOB,LED_ANSB);
+
+  while (true) {
+      palTogglePad(GPIOA,LED_TRUE);
+      palTogglePad(GPIOA,LED_FALSE);
+      palTogglePad(GPIOA,LED_ANSA);
+      palTogglePad(GPIOB,LED_ANSB);
+
+      chThdSleepMilliseconds(500);
+  }
+}
+
+static void indicator_start(void){
+    palSetPadMode(GPIOA,LED_TRUE,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOA,LED_FALSE,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOA,LED_ANSA,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOB,LED_ANSB,PAL_MODE_OUTPUT_PUSHPULL);
+
+    palSetPadMode(GPIOA,LED_AMPL,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOA,LED_FREQ,PAL_MODE_OUTPUT_PUSHPULL);
+
+    palSetPadMode(GPIOB,LED_M1,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOB,LED_M2,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOB,LED_M3,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOB,LED_M4,PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOB,LED_M5,PAL_MODE_OUTPUT_PUSHPULL);
+
+    palSetPad(GPIOA,LED_AMPL);
+    indicator_m_on(1);
+
+    //chThdCreateStatic(waIndicator, sizeof(waIndicator),	NORMALPRIO, thdIndicator, NULL);
+    chThdCreateStatic(waTestLed, sizeof(waTestLed),	NORMALPRIO, thdTestLed, NULL);
+}
+
+//================================================================================
+
 // ============================= Platform Calling ===================
 
 void system_init(void){
 	halInit();
 	chSysInit();
-	
-	palSetPadMode(GPIOC, 13, PAL_MODE_INPUT_PULLUP);
-	extStart(&EXTD1, &extcfg);
-	extChannelEnable(&EXTD1, 13);
 
+    exti_start();
+    indicator_start();
 	led_start();
 }
 
