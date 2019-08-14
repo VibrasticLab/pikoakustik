d600cc63ad6851d9ba41615d11a308d70f9e8f27
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-08-15 05:51:01 +0700

    wave generation details

--- singraf.py
+++ singraf.py
@@ -7,23 +7,22 @@ import matplotlib.pyplot as plt
 NUM_SAMPLES = 512
 SAMPLING_RATE = 44100
 
-def sample_prep(FR, DUR, AMP):
+# one wave length
+def onewavelen(FR,AMP):
 	x = 0
 	y = 0
-
+	
 	neg_a = 0
 	neg_b = 0
 	phase = 0
 	stop = 0
 	sample = 0
-	numwave = 0
-
-	i = 1
 	
-	sine_wave = np.zeros(NUM_SAMPLES)
 	sine_sample = np.zeros(NUM_SAMPLES)
 	sine_sample[0] = AMP * 0.2
-
+	
+	i = 1
+	
 	while stop == 0:
 		x = i / SAMPLING_RATE
 		y = mt.sin(2.0 * mt.pi * FR * x ) + 1
@@ -50,10 +49,35 @@ def sample_prep(FR, DUR, AMP):
 		if i == NUM_SAMPLES:
 			stop = 1
 	
-	return sine_sample,i
-            
+	print("one wave len = %i" % i)
+	return i
 
-datasin,arrsize = sample_prep(1000,2,10000)
-print("arraysize = %i" % arrsize)
+# build wave sample
+def sample_prep(FR, DUR, AMP):
+	x = 0
+	y = 0
+	
+	waveone = onewavelen(FR,AMP)
+	wavenum = int(NUM_SAMPLES/waveone)
+	wavelen = wavenum * waveone
+
+	sample = 0
+	
+	sine_sample = np.zeros(NUM_SAMPLES)
+	sine_sample[0] = AMP * 0.2
+
+	for i in range(1,NUM_SAMPLES):
+		x = i / SAMPLING_RATE
+		y = mt.sin(2.0 * mt.pi * FR * x ) + 1
+		sample = AMP * 0.2 * y
+
+		sine_sample[i] = sample
+		i = i + 1
+
+	print("wave len = %i * %i = %i" % (wavenum,waveone,wavelen))
+	wave_sample = sine_sample[0:wavelen+1]
+	return wave_sample
+         
+datasin = sample_prep(1000,2,10000)
 plt.plot(datasin)
 plt.show()
