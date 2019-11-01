c19ffcad728dfcbd7df359128235c704e1b1fa11
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2019-11-01 13:58:34 +0700

    Start implement MMC

---
+++ nucleo401/ht_mmc.h
@@ -0,0 +1,47 @@
+/*
+              UNKNOWN PUBLIC LICENSE
+
+ Copyright (C) 2019 Achmadi S.T. M.T.
+
+ Currently no license applied because author liv in
+ Indonesia, a country which doesn't really concern
+ about digital content copyright.
+
+ */
+
+/**
+ * @file    ht_mmc.h
+ * @brief   MMC-SPI FAT-FS header.
+ *
+ * @addtogroup Storage
+ * @{
+ */
+
+#ifndef HT_MMC_H
+#define HT_MMC_H
+
+#include <stdlib.h>
+#include <string.h>
+
+#define MMC_SPI_OK      0
+#define MMC_SPI_FAIL    1
+#define MMC_SPI_ERROR   2
+
+/**
+ * @brief Define whether use checking routine or not
+ * @details Highly recommended to enabled it
+ */
+#define USE_MMC_CHK 1
+
+/**
+ * @brief Test Write and Read Simple text in Append mode
+ */
+void ht_mmc_Test(void);
+
+/**
+ * @brief Initialize MMC-SPI peripherals
+ */
+void ht_mmc_Init(void);
+
+#endif // HT_MMC_H
+/** @} */
