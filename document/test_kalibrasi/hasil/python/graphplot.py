52921c954401d1131ee4d8b8e40ed1e2d5f94f22
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2020-08-03 15:31:36 +0000

    dokumen laporan progres

--- document/test_kalibrasi/hasil/python/graphplot.py
+++ document/test_kalibrasi/hasil/python/graphplot.py
@@ -4,7 +4,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-outFolder = 'kenion'
+outFolder = 'bose'
 outFreq = [250,500,1000,2000,4000,8000]
 outAmpl = [0.0039,0.0078,0.0156,0.0312,0.0625,0.125,0.25,0.5,1]
 
