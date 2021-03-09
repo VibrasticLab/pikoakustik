31b18dc1045d682c7fa1ec326f16bebfaa690330
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-03-09 11:45:01 +0700

    android iface

--- android/serialInterface/serIface/lib/main.dart
+++ android/serialInterface/serIface/lib/main.dart
@@ -1,117 +1,180 @@
 import 'package:flutter/material.dart';
+import 'dart:typed_data';
+import 'dart:async';
+import 'package:usb_serial/usb_serial.dart';
+import 'package:usb_serial/transaction.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class myApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _myAppState createState() => _myAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class _myAppState extends State<myApp> {
+  UsbPort _port;
+  String _status = "Idle";
+  List<Widget> _ports = [];
+  List<Widget> _serialData = [];
+  StreamSubscription<String> _subscription;
+  Transaction<String> _transaction;
+  int _deviceId;
+  TextEditingController _textController = TextEditingController();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Future<bool> _connectTo(device) async {
+    _serialData.clear();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    if (_subscription != null) {
+      _subscription.cancel();
+      _subscription = null;
+    }
 
-  final String title;
+    if (_transaction != null) {
+      _transaction.dispose();
+      _transaction = null;
+    }
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    if (_port != null) {
+      _port.close();
+      _port = null;
+    }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    if (device == null) {
+      _deviceId = null;
 
-  void _incrementCounter() {
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (!await _port.open()) {
+      setState(() {
+        _status = "Open Failed";
+      });
+      return false;
+    }
+
+    _deviceId = device.deviceId;
+    await _port.setDTR(false);
+    await _port.setRTS(false);
+    await _port.setPortParameters(
+        9600, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _transaction = Transaction.stringTerminated(
+        _port.inputStream, Uint8List.fromList([13, 10]));
+
+    _subscription = _transaction.stream.listen((String line) {
+      setState(() {
+        _serialData.add(Text(line));
+        if (_serialData.length > 20) {
+          _serialData.removeAt(0);
+        }
+      });
+    });
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _status = "Connected";
+    });
+    return true;
+  }
+
+  void _getPorts() async {
+    _ports = [];
+
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    print(devices);
+
+    devices.forEach((device) {
+      _ports.add(ListTile(
+        leading: Icon(Icons.usb),
+        title: Text(device.productName),
+        subtitle: Text(device.manufacturerName),
+        trailing: RaisedButton(
+          child: Text(_deviceId == device.deviceId ? "Disconnect" : "Connect"),
+          onPressed: () {
+            _connectTo(_deviceId == device.deviceId ? null : device)
+                .then((res) {
+              _getPorts();
+            });
+          },
+        ),
+      ));
+    });
+
+    setState(() {
+      print(_ports);
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    UsbSerial.usbEventStream.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('USB Serial'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(
+                _ports.length > 0 ? "Serial Available" : "No USB Serial",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              ..._ports,
+              Text('Status: $_status\n'),
+              ListTile(
+                title: TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Text To Send',
+                  ),
+                ),
+                trailing: RaisedButton(
+                  child: Text("Send"),
+                  onPressed: _port == null
+                      ? null
+                      : () async {
+                          if (_port == null) {
+                            return;
+                          }
+
+                          String data = _textController.text + "\r\n";
+                          await _port.write(Uint8List.fromList(data.codeUnits));
+                          _textController.text = "";
+                        },
+                ),
+              ),
+              Text(
+                "Result",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              ..._serialData,
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
