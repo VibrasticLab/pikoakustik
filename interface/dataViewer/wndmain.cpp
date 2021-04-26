72045890b4ada1e46e09d02645252698ff4f77e7
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-04-26 23:46:05 +0700

    dataViewer

--- interface/dataViewer/wndmain.cpp
+++ interface/dataViewer/wndmain.cpp
@@ -9,7 +9,37 @@ wndMain::wndMain(QWidget *parent)
 
     ui->plotMain->setCanvasBackground(Qt::white);
     ui->plotMain->setAxisScale(QwtPlot::yLeft,0,110,10);
-    ui->plotMain->setAxisScale(QwtPlot::xBottom,0,15,1);
+    ui->plotMain->setAxisScale(QwtPlot::xBottom,1,12,1);
+    ui->plotMain->insertLegend(new QwtLegend());
+
+    QwtPlotGrid *grid = new QwtPlotGrid();
+    grid->attach(ui->plotMain);
+
+    QwtPlotCurve *curve = new QwtPlotCurve();
+    curve->setTitle("Record");
+    curve->setPen(Qt::blue,4);
+    curve->setRenderHint(QwtPlotItem::RenderAntialiased,true);
+
+    QwtSymbol *symbol = new QwtSymbol(
+            QwtSymbol::Ellipse,
+            QBrush(Qt::yellow),
+            QPen(Qt::red,2),
+            QSize(8,8) );
+    curve->setSymbol(symbol);
+
+    QPolygonF points;
+    points  << QPointF(1.0, 72.9)
+            << QPointF(2.0, 66.9)
+            << QPointF(3.0, 60.8)
+            << QPointF(4.0, 54.8)
+            << QPointF(5.0, 49.0)
+            << QPointF(6.0, 43.5)
+            << QPointF(7.0, 39.1)
+            << QPointF(8.0, 36.3)
+            << QPointF(9.0, 34.9);
+
+    curve->setSamples(points);
+    curve->attach(ui->plotMain);
 
     addSerialPort();
 }
@@ -37,3 +67,12 @@ void wndMain::addSerialPort(void){
 #endif
 }
 
+
+void wndMain::on_btnDataBrowse_clicked()
+{
+    dataFile = QFileDialog::getOpenFileName(this,
+                                            "Open Record File",
+                                            "",
+                                            ("TEXT (*.TXT)") );
+    ui->txtDataFile->setText(dataFile);
+}
--- interface/dataViewer/wndmain.h
+++ interface/dataViewer/wndmain.h
@@ -2,6 +2,13 @@
 #define WNDMAIN_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <qwt_plot_grid.h>
+#include <qwt_symbol.h>
+#include <qwt_legend.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class wndMain; }
@@ -15,9 +22,12 @@ public:
     wndMain(QWidget *parent = nullptr);
     ~wndMain();
 
+private slots:
+    void on_btnDataBrowse_clicked();
+
 private:
     Ui::wndMain *ui;
-
+    QString dataFile;
     void addSerialPort(void);
 };
 #endif // WNDMAIN_H
