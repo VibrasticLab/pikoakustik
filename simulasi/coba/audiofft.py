af478ee4f5345577888a3dbbde0c825f9b3182d0
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-06-14 22:10:27 +0700

    much better

--- simulasi/coba/audiofft.py
+++ simulasi/coba/audiofft.py
@@ -7,38 +7,40 @@ from scipy import fftpack
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+import matplotlib
+matplotlib.use('GTK3Cairo')
+
 def update_data(i):
 	fs = 44100
-	#duration = 0.1
-	#mycapture = sd.rec(int(duration*fs),channels=2)
-
-	mycapture = sd.rec(1024,channels=2)
+	mycapture = sd.rec(4096,channels=2)
 	sd.wait()
 
-	splsL = fftpack.fft(mycapture[:,0])
-	freqs = fftpack.fftfreq(len(mycapture[:,0])) * fs
-	splsR = fftpack.fft(mycapture[:,1])
+	spl_L = np.fft.rfft(mycapture[:,0])
+	freqs = np.fft.rfftfreq(len(mycapture[:,0])) * fs
+	spl_R = np.fft.rfft(mycapture[:,1])
 
-	axL.cla()
-	axL.loglog(freqs,np.abs(splsL))
-	axL.grid(True,which='both',ls='-')
-	axL.set_xlim([32,10000])
-	axL.set_ylim([0.01,10000])
-
-	axR.cla()
-	axR.loglog(freqs,np.abs(splsR))
-	axR.grid(True,which='both',ls='-')
-	axR.set_xlim([32,10000])
-	axR.set_ylim([0.01,10000])
+	lineL.set_data(freqs,np.abs(spl_L))
+	lineR.set_data(freqs,np.abs(spl_R))
 
 sd.default.device = 2, None
 sd.default.channels = 2, None
 #print(sd.query_devices())
 
-fig = plt.figure(figsize=(12,6), facecolor='#DEDEDE')
+fig = plt.figure(figsize=(8,4), facecolor='#DEDEDE')
+
 axL = plt.subplot(211)
-axR = plt.subplot(212)
 axL.set_facecolor('#DEDEDE')
+axL.grid(True,which='both',ls='-')
+axL.set_xlim([32,10000])
+axL.set_ylim([0.1,1000])
+lineL, = axL.loglog([1])
+
+axR = plt.subplot(212)
 axR.set_facecolor('#DEDEDE')
+axR.grid(True,which='both',ls='-')
+axR.set_xlim([32,10000])
+axR.set_ylim([0.1,1000])
+lineR, = axR.loglog([1])
+
 ani = FuncAnimation(fig, update_data, interval=1)
 plt.show()
